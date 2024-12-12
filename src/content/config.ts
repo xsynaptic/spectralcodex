d716044b4820332f9e9ffc7a084c3f6364deb866
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2024-12-13 00:58:28 +0800

    chore: add content components and files, adjust settings

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { ephemera } from '@/lib/collections/ephemera/config';
+import { images } from '@/lib/collections/images/config';
+import { locations } from '@/lib/collections/locations/config';
+import { pages } from '@/lib/collections/pages/config';
+import { posts } from '@/lib/collections/posts/config';
+import { regions } from '@/lib/collections/regions/config';
+import { series } from '@/lib/collections/series/config';
+import { themes } from '@/lib/collections/themes/config';
+
+export const collections = {
+	ephemera,
+	images,
+	locations,
+	pages,
+	posts,
+	regions,
+	series,
+	themes,
+};
