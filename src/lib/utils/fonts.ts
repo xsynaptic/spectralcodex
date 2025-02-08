fa6a7456bbc8ade5fd50306af33df432a9b88ae0
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-02-08 13:18:04 +0800

    feat: upgrade to tailwind 4 and reorganize css files

---
+++ src/lib/utils/fonts.ts
@@ -0,0 +1,57 @@
+/**
+ * Several traditional Chinese font stacks collected over the years
+ * These are probably more than a little outdated as of 2025, kept here for reference
+ */
+export const systemFontStacksChinese = {
+	// Songti: similar to serif; think Times New Roman
+	zhSongtiSc: [
+		'Songti SC',
+		'STSong',
+		'华文宋体',
+		'宋体',
+		'SimSun',
+		'新宋体',
+		'NSimSun',
+		'AR PL New Sung',
+		'AR PL SungtiL GB',
+	],
+	zhSongtiTc: [
+		'Songti TC',
+		'LiSong Pro',
+		'Apple LiSung',
+		'新細明體',
+		'PMingLiU',
+		'MingLiU',
+		'AR PL Mingti2L',
+		'TW-Sung',
+	],
+	// Heiti: similar to sans-serif; think Helvetica/Arial
+	zhHeitiSc: [
+		'Heiti SC',
+		'Microsoft YaHei New',
+		'Microsoft Yahei',
+		'微软雅黑',
+		'SimHei',
+		'黑体',
+		'STHeiti Light',
+		'STXihei',
+		'华文细黑',
+		'STHeiti',
+		'华文黑体',
+		'WenQuanYi Zen Hei',
+	],
+	zhHeitiTc: ['Noto Sans TC', 'Heiti TC', 'Microsoft JhengHei', 'PingFang TC', '微軟正黑體'],
+	// Kaiti: a regular brush script
+	zhKaitiSc: ['Kaiti SC', 'KaiTi', '楷体', 'STKaiti', '华文楷体', 'Kai', 'AR PL UKai CN'],
+	zhKaitiTc: [
+		'BiauKai',
+		'DFKai-SB',
+		'AR PL KaitiM',
+		'AR PL KaitiM GB',
+		'AR PL UKai HK',
+		'AR PL UKai TW',
+		'TW-Kai',
+	],
+	// Fangsongti: stylized brush script; a fusion of kaiti and songti styles; only available for simplified Chinese insofar as I can tell
+	zhFangsongSc: ['FangSong', 'Fang Song', '仿宋', 'STFangSong', '华文仿宋'],
+};
