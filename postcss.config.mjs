49e94a284f4e295dbf5195388709e77eb0385cd2
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-02-08 13:33:59 +0800

    chore: update dependencies, version bump

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,5 @@
 import postcssInlineSvg from 'postcss-inline-svg';
 
-// Note: Astro's Tailwind integration includes Autoprefixer
 export default {
 	plugins: [postcssInlineSvg({ paths: ['./src/styles/svg'], removeFill: true })],
 };
