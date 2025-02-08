38fec7587213e7e3ee4de0264138243ced69ff6f
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-02-08 13:14:25 +0800

    chore: remove tailwind configuration package

--- packages/tailwind/src/config.ts
+++
@@ -1,8 +0,0 @@
-// A small utility for resolving the actual config being used in this project
-import resolveConfig from 'tailwindcss/resolveConfig';
-
-import tailwindConfig from '../../../tailwind.config';
-
-const customTailwindConfig = resolveConfig(tailwindConfig);
-
-export { customTailwindConfig as tailwindConfig };
--- packages/tailwind/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { backgroundImage } from './background-image';
-export { colors } from './colors';
-export { fontFamily } from './font-family';
-export { typography } from './typography';
