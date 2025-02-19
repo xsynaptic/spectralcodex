d21ec89c1121889f8dc31ca53528c8264d161feb
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-02-19 20:53:22 +0800

    feat: simplified image collection while transitioning to remote approach

---
+++ src/lib/collections/images/config-local.ts
@@ -0,0 +1,109 @@
+import { imageLoader } from '@spectralcodex/image-loader';
+import { defineCollection, z } from 'astro:content';
+import { ExifTool } from 'exiftool-vendored';
+
+import { CONTENT_MEDIA_PATH, FEATURE_IMAGE_METADATA } from '@/constants';
+import {
+	getImageExposureValue,
+	getImageFileUrlPlaceholder,
+	getImageTitle,
+} from '@/lib/image/image-loader-utils';
+import { GeometrySchema } from '@/lib/schemas/geometry';
+import { getLocalImageTransformFunction } from '@/lib/schemas/image';
+
+// Note: this file contains a more full-fledged image loader example
+// Because we are not using EXIF data in this project it is not currently in use
+const ImageMetadataSchema = z.object({
+	title: z.string().optional(),
+	titleAlt: z.string().optional(),
+	titleRaw: z.string().optional(),
+	dateCaptured: z.date().optional(),
+	brand: z.string().optional(),
+	camera: z.string().optional(),
+	lens: z.string().optional(),
+	aperture: z.string().optional(),
+	shutterSpeed: z.string().optional(),
+	focalLength: z.string().optional(),
+	iso: z.string().optional(),
+	exposureValue: z.string().optional(),
+	geometry: GeometrySchema.optional(),
+	placeholder: z.string().optional(),
+});
+
+type ImageMetadataInput = z.input<typeof ImageMetadataSchema>;
+
+let exiftool: ExifTool;
+
+export const images = defineCollection({
+	loader: imageLoader({
+		base: CONTENT_MEDIA_PATH,
+		concurrency: 80,
+		dataHandler: async ({ filePathRelative, fileUrl, logger }) => {
+			const placeholder = await getImageFileUrlPlaceholder({
+				fileUrl,
+				onError: (errorMessage) => {
+					logger.error(errorMessage);
+				},
+				onNotFound: (errorMessage) => {
+					logger.warn(errorMessage);
+				},
+			});
+
+			const metadata = FEATURE_IMAGE_METADATA
+				? await (async () => {
+						const tags = await exiftool.read(filePathRelative);
+
+						const dateCaptured = tags.DateCreated ? tags.DateCreated.toString() : undefined;
+
+						return {
+							...getImageTitle(tags.Title),
+							titleRaw: String(tags.Title),
+							dateCaptured: dateCaptured ? new Date(dateCaptured) : undefined,
+							brand: String(tags.Make),
+							camera: String(tags.Model),
+							lens: tags.LensID ? String(tags.LensID) : String(tags.LensModel),
+							aperture: String(tags.FNumber),
+							shutterSpeed: String(tags.ShutterSpeed),
+							focalLength: String(tags.FocalLength),
+							iso: String(tags.ISO),
+							exposureValue: getImageExposureValue({
+								aperture: String(tags.FNumber),
+								shutterSpeed: String(tags.ShutterSpeed),
+							}),
+							...(tags.GPSLatitude && tags.GPSLongitude
+								? {
+										geometry: {
+											type: 'Point' as const,
+											coordinates: [Number(tags.GPSLongitude), Number(tags.GPSLatitude)] as [
+												number,
+												number,
+											],
+										},
+									}
+								: {}),
+						};
+					})()
+				: {};
+
+			return {
+				placeholder,
+				...metadata,
+			} satisfies ImageMetadataInput;
+		},
+		...(FEATURE_IMAGE_METADATA
+			? {
+					beforeLoad: () => {
+						exiftool = new ExifTool({ ignoreZeroZeroLatLon: true });
+					},
+					afterLoad: () => {
+						void exiftool.end();
+					},
+				}
+			: {}),
+	}),
+	schema: ({ image: imageFunction }) =>
+		ImageMetadataSchema.extend({
+			src: getLocalImageTransformFunction({ imageFunction, defaultPath: '' }),
+			modifiedTime: z.date().optional(),
+		}).strict(),
+});
--- src/lib/collections/images/config.ts
+++ src/lib/collections/images/config.ts
@@ -1,107 +1,39 @@
 import { imageLoader } from '@spectralcodex/image-loader';
 import { defineCollection, z } from 'astro:content';
-import { ExifTool } from 'exiftool-vendored';
+import sharp from 'sharp';
 
-import { CONTENT_MEDIA_PATH, FEATURE_IMAGE_METADATA } from '@/constants';
-import {
-	getImageExposureValue,
-	getImageFileUrlPlaceholder,
-	getImageTitle,
-} from '@/lib/image/image-loader-utils';
-import { GeometrySchema } from '@/lib/schemas/geometry';
-import { getLocalImageTransformFunction } from '@/lib/schemas/image';
+import { CONTENT_MEDIA_HOST, CONTENT_MEDIA_PATH } from '@/constants';
 
 const ImageMetadataSchema = z.object({
-	title: z.string().optional(),
-	titleAlt: z.string().optional(),
-	titleRaw: z.string().optional(),
-	dateCaptured: z.date().optional(),
-	brand: z.string().optional(),
-	camera: z.string().optional(),
-	lens: z.string().optional(),
-	aperture: z.string().optional(),
-	shutterSpeed: z.string().optional(),
-	focalLength: z.string().optional(),
-	iso: z.string().optional(),
-	exposureValue: z.string().optional(),
-	geometry: GeometrySchema.optional(),
+	src: z.string(),
+	path: z.string(),
+	width: z.number(),
+	height: z.number(),
+	modifiedTime: z.date().optional(),
 	placeholder: z.string().optional(),
 });
 
 type ImageMetadataInput = z.input<typeof ImageMetadataSchema>;
 
-let exiftool: ExifTool;
+async function getImageDimensions(imagePath: string) {
+	const metadata = await sharp(imagePath).metadata();
+
+	return { width: metadata.width, height: metadata.height };
+}
 
 export const images = defineCollection({
 	loader: imageLoader({
 		base: CONTENT_MEDIA_PATH,
 		concurrency: 80,
-		dataHandler: async ({ filePathRelative, fileUrl, logger }) => {
-			const placeholder = await getImageFileUrlPlaceholder({
-				fileUrl,
-				onError: (errorMessage) => {
-					logger.error(errorMessage);
-				},
-				onNotFound: (errorMessage) => {
-					logger.warn(errorMessage);
-				},
-			});
-
-			const metadata = FEATURE_IMAGE_METADATA
-				? await (async () => {
-						const tags = await exiftool.read(filePathRelative);
-
-						const dateCaptured = tags.DateCreated ? tags.DateCreated.toString() : undefined;
-
-						return {
-							...getImageTitle(tags.Title),
-							titleRaw: String(tags.Title),
-							dateCaptured: dateCaptured ? new Date(dateCaptured) : undefined,
-							brand: String(tags.Make),
-							camera: String(tags.Model),
-							lens: tags.LensID ? String(tags.LensID) : String(tags.LensModel),
-							aperture: String(tags.FNumber),
-							shutterSpeed: String(tags.ShutterSpeed),
-							focalLength: String(tags.FocalLength),
-							iso: String(tags.ISO),
-							exposureValue: getImageExposureValue({
-								aperture: String(tags.FNumber),
-								shutterSpeed: String(tags.ShutterSpeed),
-							}),
-							...(tags.GPSLatitude && tags.GPSLongitude
-								? {
-										geometry: {
-											type: 'Point' as const,
-											coordinates: [Number(tags.GPSLongitude), Number(tags.GPSLatitude)] as [
-												number,
-												number,
-											],
-										},
-									}
-								: {}),
-						};
-					})()
-				: {};
+		dataHandler: async ({ id, filePathRelative }) => {
+			const dimensions = await getImageDimensions(filePathRelative);
 
 			return {
-				placeholder,
-				...metadata,
+				src: `${CONTENT_MEDIA_HOST}/${id}`,
+				path: filePathRelative,
+				...Object.assign({ width: 1200, height: 900 }, dimensions),
 			} satisfies ImageMetadataInput;
 		},
-		...(FEATURE_IMAGE_METADATA
-			? {
-					beforeLoad: () => {
-						exiftool = new ExifTool({ ignoreZeroZeroLatLon: true });
-					},
-					afterLoad: () => {
-						void exiftool.end();
-					},
-				}
-			: {}),
 	}),
-	schema: ({ image: imageFunction }) =>
-		ImageMetadataSchema.extend({
-			src: getLocalImageTransformFunction({ imageFunction, defaultPath: '' }),
-			modifiedTime: z.date().optional(),
-		}).strict(),
+	schema: ImageMetadataSchema.strict(),
 });
