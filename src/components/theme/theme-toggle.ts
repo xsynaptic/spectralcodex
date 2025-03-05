ecaaa24e153f0402e5055e486b0474ccc97cd6f9
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-03-05 20:29:28 +0800

    chore: debounce theme toggle

--- src/components/theme/theme-toggle.ts
+++ src/components/theme/theme-toggle.ts
@@ -1,3 +1,5 @@
+import { funnel } from 'remeda';
+
 import { ThemeTypeEnum } from '@/components/theme/theme-types';
 
 export function activateThemeToggle(toggleElementId: string) {
@@ -5,25 +7,33 @@ export function activateThemeToggle(toggleElementId: string) {
 
 	if (!toggleElement) return;
 
+	// Debounce toggle that prevents rapid flicking between modes
+	const debouncedThemeToggle = funnel(
+		() => {
+			if (!window.theme) return;
+
+			const currentTheme = window.theme.getTheme();
+
+			switch (currentTheme) {
+				case ThemeTypeEnum.Auto:
+				case ThemeTypeEnum.Light: {
+					window.theme.setTheme(ThemeTypeEnum.Dark);
+					break;
+				}
+				case ThemeTypeEnum.Dark: {
+					window.theme.setTheme(ThemeTypeEnum.Light);
+					break;
+				}
+				default: {
+					currentTheme satisfies never;
+					break;
+				}
+			}
+		},
+		{ triggerAt: 'start', minQuietPeriodMs: 500 },
+	);
+
 	toggleElement.addEventListener('click', () => {
-		if (!window.theme) return;
-
-		const currentTheme = window.theme.getTheme();
-
-		switch (currentTheme) {
-			case ThemeTypeEnum.Auto:
-			case ThemeTypeEnum.Light: {
-				window.theme.setTheme(ThemeTypeEnum.Dark);
-				break;
-			}
-			case ThemeTypeEnum.Dark: {
-				window.theme.setTheme(ThemeTypeEnum.Light);
-				break;
-			}
-			default: {
-				currentTheme satisfies never;
-				break;
-			}
-		}
+		debouncedThemeToggle.call();
 	});
 }
