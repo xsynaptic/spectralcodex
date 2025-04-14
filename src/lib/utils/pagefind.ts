e4fc11462270ae32a89380e18e46ef46fe803be7
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-04-14 10:20:37 +0800

    chore: pagefind body props function simplification

--- src/lib/utils/pagefind.ts
+++ src/lib/utils/pagefind.ts
@@ -1,12 +1,5 @@
-interface CollectionEntryWithHideSearch {
-	data: {
-		hideSearch?: boolean | undefined;
-	};
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-export function getPagefindBodyProp<T extends CollectionEntryWithHideSearch>(entry: T) {
-	if (entry.data.hideSearch) {
+export function getPagefindBodyProp(hideSearch?: boolean) {
+	if (hideSearch) {
 		return {};
 	}
 	return { 'data-pagefind-body': '' };
