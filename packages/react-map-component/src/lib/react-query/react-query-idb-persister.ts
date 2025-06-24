a3e7d3d23f0efab09221281e5f6b32d0fca6a544
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-06-24 20:34:14 +0800

    feat: persist react query data to indexeddb

---
+++ packages/react-map-component/src/lib/react-query/react-query-idb-persister.ts
@@ -0,0 +1,21 @@
+import type { PersistedClient, Persister } from '@tanstack/react-query-persist-client';
+
+import { del, get, set } from 'idb-keyval';
+
+/**
+ * Creates an Indexed DB persister
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
+ */
+export function createIdbPersister(idbValidKey: IDBValidKey = 'reactQuery') {
+	return {
+		persistClient: async (client: PersistedClient) => {
+			await set(idbValidKey, client);
+		},
+		restoreClient: async () => {
+			return await get<PersistedClient>(idbValidKey);
+		},
+		removeClient: async () => {
+			await del(idbValidKey);
+		},
+	} satisfies Persister;
+}
--- packages/react-map-component/src/lib/react-query/react-query-provider.tsx
+++ packages/react-map-component/src/lib/react-query/react-query-provider.tsx
@@ -1,15 +1,16 @@
 import type { PropsWithChildren } from 'react';
 
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { persistQueryClient, removeOldestQuery } from '@tanstack/react-query-persist-client';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { useState } from 'react';
+
+import { createIdbPersister } from './react-query-idb-persister';
 
 const TIME_24_HOURS = 1000 * 60 * 60 * 24;
 
 export const ReactQueryProvider = ({ children }: PropsWithChildren) => {
 	const [queryClient] = useState(() => {
-		const client = new QueryClient({
+		return new QueryClient({
 			defaultOptions: {
 				queries: {
 					staleTime: TIME_24_HOURS,
@@ -17,19 +18,17 @@ export const ReactQueryProvider = ({ children }: PropsWithChildren) => {
 				},
 			},
 		});
-
-		void persistQueryClient({
-			queryClient: client,
-			persister: createSyncStoragePersister({
-				key: 'scx-map-data-cache',
-				storage: globalThis.localStorage,
-				retry: removeOldestQuery,
-			}),
-			maxAge: TIME_24_HOURS,
-		});
-
-		return client;
 	});
 
-	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+	return (
+		<PersistQueryClientProvider
+			client={queryClient}
+			persistOptions={{
+				persister: createIdbPersister('scx-map-data-cache'),
+				maxAge: TIME_24_HOURS,
+			}}
+		>
+			{children}
+		</PersistQueryClientProvider>
+	);
 };
