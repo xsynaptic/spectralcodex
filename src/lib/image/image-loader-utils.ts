ab9c3863b4379dd458401b1a8346d4ccd9db1576
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-07-27 00:53:58 +0800

    chore: minor adjustments

--- src/lib/image/image-loader-utils.ts
+++ src/lib/image/image-loader-utils.ts
@@ -1,31 +1,8 @@
-import { wrapCjk } from '@xsynaptic/unified-tools';
 import { promises as fs } from 'node:fs';
 import sharp from 'sharp';
 
 import { IMAGE_PLACEHOLDER_PIXEL_COUNT_LQ } from '#constants.ts';
 import { getImagePlaceholderDataUrl } from '#lib/image/image-placeholder.ts';
-
-// Split raw titles in the format `English Title (中文)`
-export function getImageTitle(titleRaw: string | undefined) {
-	const titleWrapped = titleRaw ? wrapCjk(titleRaw) : undefined;
-
-	if (titleWrapped) {
-		const matches = [...titleWrapped.matchAll(/<span lang="zh">(.*?)<\/span>/g)][0];
-
-		// TODO: update this for multilingual use
-		return {
-			title: titleWrapped
-				.replaceAll(/<span lang="zh">.*?<\/span>/g, '')
-				.replaceAll(' ()', '') // Sometimes left over after Chinese characters are removed
-				.trim(),
-			titleAlt: matches ? matches.at(1)?.trim() : undefined,
-		};
-	}
-	return {
-		title: undefined,
-		titleAlt: undefined,
-	};
-}
 
 // Calculate EV using the formula EV = log2(N^2 / t)
 export function getImageExposureValue({
--- src/lib/map/map-type-guards.ts
+++ src/lib/map/map-type-guards.ts
@@ -2,13 +2,18 @@ import type { MapGeometry } from '@spectralcodex/react-map-component';
 import type { Feature, FeatureCollection } from 'geojson';
 import type { LngLatBoundsLike } from 'maplibre-gl';
 
-export const isGeojsonFeature = <T extends MapGeometry, P>(
+export function isGeojsonFeature<T extends MapGeometry, P>(
 	input: Feature<T, P> | FeatureCollection | undefined,
-): input is Feature<T, P> => !!input && input.type === 'Feature';
+): input is Feature<T, P> {
+	return !!input && input.type === 'Feature';
+}
 
 // GeoJSON bounding boxes can have 6 items in the array; MapLibre only supports 4
-export const isLngLatBoundsLike = (input: unknown): input is LngLatBoundsLike =>
-	!!input &&
-	Array.isArray(input) &&
-	input.length === 4 &&
-	input.every((item) => typeof item === 'number');
+export function isLngLatBoundsLike(input: unknown): input is LngLatBoundsLike {
+	return (
+		!!input &&
+		Array.isArray(input) &&
+		input.length === 4 &&
+		input.every((item) => typeof item === 'number')
+	);
+}
