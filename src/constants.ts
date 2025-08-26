0f042a7ae644b415ff8119e47d70a8e90a0463bf
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-08-27 01:50:47 +0800

    feat: image metadata refinement

--- src/constants.ts
+++ src/constants.ts
@@ -85,7 +85,7 @@ export const SITE_YEAR_FOUNDED = 2009;
 export const FEATURE_DATE_ARCHIVES = false as boolean;
 
 // Image metadata generation; set to "true" to enable
-export const FEATURE_IMAGE_METADATA = false as boolean;
+export const FEATURE_IMAGE_METADATA = true as boolean;
 
 // Nearby items are the single most expensive data generated in this project
 export const FEATURE_LOCATION_NEARBY_ITEMS = import.meta.env.PROD;
