67156677cd8a8ee06e9f36c21f4cbab6f15e7bd0
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-11-02 12:33:46 +0800

    feat: improve map control styling

--- packages/react-map-component/src/controls/controls-metrics.tsx
+++ packages/react-map-component/src/controls/controls-metrics.tsx
@@ -6,6 +6,8 @@ import { useSourceDataQuery } from '../data/data-source';
 import { translations } from '../lib/translations';
 import { CustomControlPortal } from './controls-custom';
 
+const formatNumber = new Intl.NumberFormat('en');
+
 export const MetricsControl: FC<{
 	position: ControlPosition;
 }> = function MetricsControl({ position }) {
@@ -13,22 +15,18 @@ export const MetricsControl: FC<{
 
 	const { isLoading: isSourceDataLoading } = useSourceDataQuery();
 
-	const formatNumber = new Intl.NumberFormat('en');
-
 	// Note: the class name is specified to remove the white background and border added by default
 	return (
 		<CustomControlPortal className="maplibregl-ctrl" position={position}>
 			{isSourceDataLoading ? (
-				<div className="flex flex-col items-end">
+				<div className="maplibregl-ctrl-metrics-loading">
 					<div className="loading-animation" style={{ width: '15px' }} />
 				</div>
 			) : (
-				<div className="maplibregl-ctrl-metrics text-primary-700 dark:text-primary-300 flex flex-nowrap gap-1 font-sans text-xs select-none">
-					<span className="drop-shadow-sm">{formatNumber.format(filteredCount)}</span>
-					<span className="text-primary-400 dark:text-primary-500 font-light drop-shadow-xs">
-						/
-					</span>
-					<span className="drop-shadow-sm">
+				<div className="maplibregl-ctrl-metrics">
+					<span>{formatNumber.format(filteredCount)}</span>
+					<span className="maplibregl-ctrl-metrics-divider">/</span>
+					<span>
 						{formatNumber.format(totalCount)} {translations.points}
 					</span>
 				</div>
