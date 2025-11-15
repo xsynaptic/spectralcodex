2e6ceb06266593448ffa3104bd4f031a66da2670
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-11-15 09:49:31 +0800

    chore: graceful related content fallback

--- src/lib/utils/related.ts
+++ src/lib/utils/related.ts
@@ -7,34 +7,49 @@ import type { ContentMetadataItem } from '#lib/types/index.ts';
 import { getContentMetadataIndex } from '#lib/metadata/metadata-index.ts';
 import { loadJsonData } from '#lib/utils/data.ts';
 
-const RelatedContentItemSchema = z.record(
-	z.string(),
-	z
-		.object({
-			id: z.string(),
-			collection: z.string(),
-			score: z.number(), // Float
-		})
-		.array(),
-);
+const RelatedContentItemSchema = z
+	.record(
+		z.string(),
+		z
+			.object({
+				id: z.string(),
+				collection: z.string(),
+				score: z.number(), // Float
+			})
+			.array(),
+	)
+	.optional();
+
+async function loadRelatedContentData() {
+	try {
+		const relatedContent = await loadJsonData(
+			path.join(CACHE_DIR, 'content-related/content-related.json'),
+		);
+
+		const relatedContentParsed = await RelatedContentItemSchema.parseAsync(relatedContent);
+
+		return relatedContentParsed;
+	} catch {
+		// We aren't really concerned about missing related content data
+		return;
+	}
+}
 
 export async function getRelatedContentFunction() {
-	const relatedContent = await loadJsonData(
-		path.join(CACHE_DIR, 'content-related/content-related.json'),
-	);
+	const relatedContentData = await loadRelatedContentData();
 
-	const relatedContentParsed = await RelatedContentItemSchema.parseAsync(relatedContent);
-
-	const metadataIndex = await getContentMetadataIndex();
+	const contentMetadataIndex = await getContentMetadataIndex();
 
 	return function getRelatedContent(id: string, threshold: number): Array<ContentMetadataItem> {
-		const related = relatedContentParsed[id];
+		if (!relatedContentData) return [];
 
-		if (!related) return [];
+		const relatedItem = relatedContentData[id];
 
-		return related
+		if (!relatedItem) return [];
+
+		return relatedItem
 			.filter((item) => item.score >= threshold)
-			.map((item) => metadataIndex.get(item.id))
+			.map((item) => contentMetadataIndex.get(item.id))
 			.filter((item) => item !== undefined);
 	};
 }
