44f313c4c3a8e47916c6a71b2eab1c0944d7c339
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-11-16 18:10:36 +0800

    chore: integrate react query persister code

--- packages/react-map-component/src/lib/react-query.tsx
+++ packages/react-map-component/src/lib/react-query.tsx
@@ -1,12 +1,30 @@
+import type { PersistedClient, Persister } from '@tanstack/react-query-persist-client';
 import type { PropsWithChildren } from 'react';
 
 import { QueryClient } from '@tanstack/react-query';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { del, get, set } from 'idb-keyval';
 import { useState } from 'react';
 
-import { createIdbPersister } from './react-query-idb-persister';
-
 const TIME_24_HOURS = 1000 * 60 * 60 * 24;
+
+/**
+ * Creates an Indexed DB persister
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
+ */
+function createIdbPersister(idbValidKey: IDBValidKey = 'reactQuery') {
+	return {
+		persistClient: async (client: PersistedClient) => {
+			await set(idbValidKey, client);
+		},
+		restoreClient: async () => {
+			return await get<PersistedClient>(idbValidKey);
+		},
+		removeClient: async () => {
+			await del(idbValidKey);
+		},
+	} satisfies Persister;
+}
 
 export const ReactQueryProvider = ({ children }: PropsWithChildren) => {
 	const [queryClient] = useState(() => {
