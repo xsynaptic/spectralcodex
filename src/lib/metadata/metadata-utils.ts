65e5efb5614401b849fc8938a94e38d029868520
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-11-27 15:14:31 +0800

    chore: integrate word count into series entry

--- src/lib/metadata/metadata-utils.ts
+++ src/lib/metadata/metadata-utils.ts
@@ -1,4 +1,4 @@
-import type { CollectionKey } from 'astro:content';
+import type { CollectionEntry, CollectionKey } from 'astro:content';
 
 import slugify from '@sindresorhus/slugify';
 
@@ -7,6 +7,26 @@ import type { ContentMetadataItem } from '#lib/types/index.ts';
 import { CONTENT_LINKS_MISSING_ID_LOG } from '#constants.ts';
 import { getContentMetadataIndex } from '#lib/metadata/metadata-index.ts';
 import { logError } from '#lib/utils/logging.ts';
+
+// Provision the content metadata getter function
+export async function getContentMetadataFunction() {
+	const contentMetadataIndex = await getContentMetadataIndex();
+
+	return function getContentMetadata<T extends CollectionKey = CollectionKey>(
+		entries: Array<CollectionEntry<T>>,
+	) {
+		return entries.map(({ id, collection }) => {
+			const contentMetadata = contentMetadataIndex.get(id);
+
+			if (!contentMetadata) {
+				throw new Error(
+					`[Metadata] Content metadata for "${id}" in the "${collection}" collection was not found!`,
+				);
+			}
+			return contentMetadata as ContentMetadataItem<T>;
+		}) satisfies Array<ContentMetadataItem<T>>;
+	};
+}
 
 export async function getContentMetadataById(id: string) {
 	const contentMetadataIndex = await getContentMetadataIndex();
