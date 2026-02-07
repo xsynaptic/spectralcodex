65d3f49c9e18624215cd52de180f406514fbe284
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2026-02-07 11:32:26 +0800

    chore: relocate render mdx function

--- src/lib/utils/rss.ts
+++ src/lib/utils/rss.ts
@@ -1,12 +1,16 @@
 import type { RSSFeedItem } from '@astrojs/rss';
+import type { ContainerRenderOptions } from 'astro/container';
 import type { CollectionEntry } from 'astro:content';
 
+import mdxRenderer from '@astrojs/mdx/server.js';
 import {
 	defaultSchema,
 	sanitizeHtml,
 	stripTags,
 	transformMarkdown,
 } from '@xsynaptic/unified-tools';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { render } from 'astro:content';
 import { performance } from 'node:perf_hooks';
 import * as R from 'remeda';
 
@@ -14,10 +18,29 @@ import { getEphemeraCollection } from '#lib/collections/ephemera/ephemera-data.t
 import { getLocationsCollection } from '#lib/collections/locations/locations-data.ts';
 import { getPostsCollection } from '#lib/collections/posts/posts-data.ts';
 import { getPrimaryMultilingualContent } from '#lib/i18n/i18n-utils.ts';
-import { getRenderMdxFunction } from '#lib/utils/astro.ts';
 import { getFilterEntryQualityFunction } from '#lib/utils/collections.ts';
 import { parseContentDate, sortByDateReverseChronological } from '#lib/utils/date.ts';
 import { getContentUrl } from '#lib/utils/routing.ts';
+
+/**
+ * Use Astro's Container API to render MDX content
+ * TODO: because there is only one container all rendering is serial; can we run multiple containers in parallel?
+ * @link https://docs.astro.build/en/reference/container-reference/
+ */
+async function getRenderMdxFunction() {
+	const container = await AstroContainer.create();
+
+	container.addServerRenderer({ name: 'mdx', renderer: mdxRenderer });
+
+	return async function (
+		entry: CollectionEntry<'ephemera' | 'locations' | 'pages' | 'posts'>,
+		options?: ContainerRenderOptions,
+	) {
+		const { Content } = await render(entry);
+
+		return await container.renderToString(Content, options);
+	};
+}
 
 /**
  * Strips GFM-style footnotes from HTML content using a simple regex approach
