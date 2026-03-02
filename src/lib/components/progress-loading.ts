28e5cf48ca4699bc48f748998bb382c755b3ec8d
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2026-03-02 19:05:49 +0800

    feat: migrate progress bars to standard web components

---
+++ src/lib/components/progress-loading.ts
@@ -0,0 +1,60 @@
+import { ProgressBar } from '#lib/components/progress-base.ts';
+
+class ProgressLoading extends ProgressBar {
+	#progress = 0.2;
+	#trickleInterval: number | undefined;
+	#thresholdTimeout: number | undefined;
+
+	#handlePreparation = () => {
+		this.#progress = 0;
+		this.setProgress(0);
+		this.#thresholdTimeout = globalThis.window.setTimeout(() => {
+			this.setOpacity(1);
+			this.#trickleInterval = globalThis.window.setInterval(() => {
+				this.#progress += Math.random() * 0.03;
+				this.setProgress(this.#progress);
+			}, this.animationDuration);
+		}, 200);
+	};
+
+	#handleSwap = () => {
+		if (!this.#thresholdTimeout) return;
+
+		globalThis.window.clearTimeout(this.#thresholdTimeout);
+		globalThis.window.clearInterval(this.#trickleInterval);
+		this.#trickleInterval = undefined;
+
+		this.#progress = 1;
+		this.setProgress(1);
+
+		globalThis.window.setTimeout(() => {
+			this.setOpacity(0);
+		}, this.animationDuration / 2);
+
+		globalThis.window.setTimeout(() => {
+			this.#progress = 0.2;
+			this.setProgress(0.2);
+		}, this.animationDuration * 2);
+	};
+
+	override connectedCallback() {
+		super.connectedCallback();
+		document.addEventListener('astro:before-preparation', this.#handlePreparation);
+		document.addEventListener('astro:before-swap', this.#handleSwap);
+	}
+
+	disconnectedCallback() {
+		document.removeEventListener('astro:before-preparation', this.#handlePreparation);
+		document.removeEventListener('astro:before-swap', this.#handleSwap);
+		globalThis.window.clearTimeout(this.#thresholdTimeout);
+		globalThis.window.clearInterval(this.#trickleInterval);
+	}
+}
+
+customElements.define('progress-loading', ProgressLoading);
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'progress-loading': ProgressLoading;
+	}
+}
