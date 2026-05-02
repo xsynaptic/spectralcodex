bd53a2dd22993ba05d9c6f0518b25eb066107349
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2026-05-02 16:53:32 +0800

    fix: progress bar css race conditions

--- src/components/progress/progress-base.ts
+++ src/components/progress/progress-base.ts
@@ -3,22 +3,6 @@ export class ProgressBar extends HTMLElement {
 
 	connectedCallback() {
 		this.ariaHidden = 'true';
-		this.style.setProperty('pointer-events', 'none');
-		this.style.setProperty('position', 'fixed');
-		this.style.setProperty('top', '0');
-		this.style.setProperty('left', '0');
-		this.style.setProperty('width', '100%');
-		this.style.setProperty('opacity', '0');
-		this.style.setProperty('transform', 'translate3d(0, 0, 0) scaleX(var(--progress-bar, 0))');
-		this.style.setProperty('transform-origin', '0');
-		this.style.setProperty(
-			'transition',
-			[
-				'background-color 100ms 100ms ease-in-out',
-				'opacity 150ms 150ms ease-in',
-				'transform 300ms ease-out',
-			].join(', '),
-		);
 	}
 
 	setProgress(value: number) {
