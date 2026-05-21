6559902c88e5c55e640465bf32bd0ecdc37ef65f
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2026-05-21 19:31:02 +0800

    chore: front-end refinement

---
+++ src/lib/utils/z-index.ts
@@ -0,0 +1,10 @@
+export const zIndexScaleEnum = {
+	ImageBase: 10,
+	ImageShadow: 11,
+	ImageHeader: 12,
+	ImageCaption: 15,
+	ImageNavigation: 15,
+	MainHeader: 40,
+	MainProgress: 60,
+	MainSkipLink: 70,
+} as const;
