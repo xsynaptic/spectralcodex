5305a9d604b3e7b986d444bee43f7ed4ec4fdb0f
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2026-05-28 13:04:32 +0800

    chore: update link platforms

--- src/lib/utils/link-platforms.ts
+++ src/lib/utils/link-platforms.ts
@@ -10,11 +10,14 @@ const linkPlatforms: Array<LinkPlatform> = [
 	{ match: ['youtube.com', 'youtu.be'], title: 'YouTube' },
 	{ match: 'x.com', title: 'X' },
 	{ match: 'instagram.com', title: 'Instagram' },
+	{ match: 'matters.town', title: 'Matters' },
 	{ match: 'threads.com', title: 'Threads' },
 	{ match: 'reddit.com', title: 'Reddit' },
 	{ match: 'hiking.biji.co', title: 'Hiking Note', title_zh: '健康筆記' },
 	{ match: 'home.gamer.com.tw', title: 'Bahamut', title_zh: '巴哈姆特' },
 	{ match: 'vimeo.com', title: 'Vimeo' },
+	{ match: 'vocus.cc', title: 'Vocus', title_zh: '方格子' },
+	{ match: 'pixnet.net', title: 'Pixnet', title_zh: '痞客邦' },
 ];
 
 export function getLinkPlatform(url: string): string | undefined {
